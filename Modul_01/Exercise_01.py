33b75d84af0ab20eb23f8f6984b1a05a42ea3939
zabrinasalsabila73@gmail.com <zabrinasalsabila73@gmail.com>
2023-03-10 09:49:32 +0700

    Exercise

---
+++ Modul_01/Exercise_01.py
@@ -0,0 +1,15 @@
+#input
+nama = "Salsabila Zabrina Apsari"
+nim = "I0322114"
+alamat = "Rendole, Pati"
+visualisasi = "terkenal dengan kota yang kaya akan hasil tanin yang kaya akan hasil tani. Selain itu juga terkenal dengan makanannya yaitu nasi gandul."
+harapan = "bisa menjadi seorang manager di perusahaan telkom"
+
+dict = {"a" : nama , "b" : nim , "c" : alamat , "d" : visualisasi , "e" : harapan}
+
+#hasil
+print("Nama saya adalah", dict["a"])
+print("Nim saya adalah", dict["b"])
+print("Saya tinggal di", dict["c"])
+print("Kota Pati adalah kota yang", dict["d"])
+print("Harapan saya sebagai lulusan Teknik Industri adalah", dict["e"])
---
+++ Modul_01/salsa.py
@@ -0,0 +1,2 @@
+nama_depan : "salsa
+nama"
